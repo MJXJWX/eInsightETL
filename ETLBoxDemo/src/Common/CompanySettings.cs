8a08646e5b7ed7b15a130b6a7b5d6e9a2ac568df
Boris <51628272@qq.com>
2019-04-22 14:29:51 +0800

    Add CompanySettings.cs

---
+++ ETLBoxDemo/src/Common/CompanySettings.cs
@@ -0,0 +1,10 @@
+namespace ETLBoxDemo.Common
+{
+    public class CompanySettings
+    {
+        public static string ETL_CENRES_SERVERNAME { get; set;}
+        public static string ETL_CENRES_DBUSER { get; set;}
+        public static string ETL_CENRES_DATABASENAME { get; set;}
+        public static string ETL_CENRES_DBPASSWORD { get; set;}
+    }
+}
