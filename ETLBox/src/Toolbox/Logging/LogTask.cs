864571c8b7b62ea1e6843901bdc92b792cc7a421
joe.chen <joe.chen@jijesoft.com>
2019-06-03 01:56:53 -0400

    Change ActionType for LogTask

--- ETLBox/src/Toolbox/Logging/LogTask.cs
+++ ETLBox/src/Toolbox/Logging/LogTask.cs
@@ -6,6 +6,7 @@
         /* ITask Interface */
         public override string TaskType { get; set; } = "LOG";
         public override string TaskName => $"Logs message";
+        public string ActionType { get; set; } = "Log";
         public override void Execute() {
             Info(Message);
         }
@@ -21,12 +22,12 @@
             Message = message; 
         }
         //NLogger.Info(TaskName, TaskType, "START", TaskHash, ControlFlow.STAGE, ControlFlow.CurrentLoadProcess?.LoadProcessKey);
-        public void Trace() => NLogger?.Trace(Message, TaskType, "LOG", TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
-        public void Debug() => NLogger?.Debug(Message, TaskType, "LOG", TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
-        public void Info() => NLogger?.Info(Message, TaskType, "LOG", TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
-        public void Warn() => NLogger?.Warn(Message, TaskType, "LOG", TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
-        public void Error() => NLogger?.Error(Message, TaskType, "LOG", TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
-        public void Fatal() => NLogger?.Fatal(Message, TaskType, "LOG", TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
+        public void Trace() => NLogger?.Trace(Message, TaskType, ActionType, TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
+        public void Debug() => NLogger?.Debug(Message, TaskType, ActionType, TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
+        public void Info() => NLogger?.Info(Message, TaskType, ActionType, TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
+        public void Warn() => NLogger?.Warn(Message, TaskType, ActionType, TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
+        public void Error() => NLogger?.Error(Message, TaskType, ActionType, TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
+        public void Fatal() => NLogger?.Fatal(Message, TaskType, ActionType, TaskHash, ControlFlow.ControlFlow.STAGE, ControlFlow.ControlFlow.CurrentLoadProcess?.LoadProcessKey);
         public static void Trace(string message) => new LogTask(message).Trace();
         public static void Debug(string message) => new LogTask(message).Debug();
         public static void Info(string message) => new LogTask(message).Info();
